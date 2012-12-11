0cf5ba067e7f70ce71bfe9bf011c2a994d2eba35
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2012-12-10 22:09:32 -0500

    starting module work

---
+++ req-count.js
@@ -0,0 +1,12 @@
+function init() {
+	console.log('computing outbound req dependencies');
+}
+
+function outbound() {
+	console.log('computing outbound links');
+}
+
+module.exports = {
+	init: init,
+	outbound: outbound
+};
--- test/basic/test.js
+++ test/basic/test.js
@@ -1,3 +1,7 @@
-var gt = ('./sure');
+var gt = require('./sure');
+var req = require('req-count');
 
 gt.module('basic tests');
+gt.test('basic', function () {
+	gt.equal(typeof req, 'object', 'req-count module exists');
+});
