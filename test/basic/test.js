f57b2b23f609122fbc1af71f7847050e147f93a5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2012-12-10 22:14:34 -0500

    simple testing

--- test/basic/test.js
+++ test/basic/test.js
@@ -1,7 +1,11 @@
-var gt = require('./sure');
-var req = require('req-count');
+var sure = require('../../node_modules/gt/sure.js');
+var req = require('../../req-count');
+
+sure.init();
 
 gt.module('basic tests');
 gt.test('basic', function () {
 	gt.equal(typeof req, 'object', 'req-count module exists');
 });
+
+sure.run();
