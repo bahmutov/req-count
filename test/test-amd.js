17d3dac1af05c704f4d3651d7a3fae09fc7839e9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2012-12-22 14:05:30 -0500

    moved test files

--- test/test-amd.js
+++ test/test-amd.js
@@ -1,5 +1,5 @@
-var sure = require('../../node_modules/gt/sure.js');
-var req = require('../../req-count');
+var sure = require('../node_modules/gt/sure.js');
+var req = require('../req-count');
 
 sure.init({
 	report: 1
@@ -16,47 +16,47 @@ gt.test('basic', function () {
 });
 
 gt.test('foo outbound', function () {
-	var out = req.outbound('foo.js');
+	var out = req.outbound('amd/foo.js');
 	gt.ok(out, 'returns an object');	
 	gt.equal(Object.keys(out).length, 1, 'there should be single outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('bar outbound', function () {
-	var out = req.outbound('./bar');
+	var out = req.outbound('amd/bar');
 	gt.ok(out, 'could not get any output');
 	gt.equal(Object.keys(out).length, 1, 'there should be nothing outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('zoo outbound', function () {
-	var out = req.outbound('./zoo');
+	var out = req.outbound('amd/zoo');
 	gt.equal(Object.keys(out).length, 1, 'there should be single outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('two outbound', function () {
-	var out = req.outbound('./two');
+	var out = req.outbound('amd/two');
 	gt.equal(Object.keys(out).length, 1, 'there should be 2 outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('multiples do not count', function () {
-	var out = req.outbound('./multiples');
+	var out = req.outbound('amd/multiples');
 	gt.equal(Object.keys(out).length, 1, 'there should be 1 outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./multiples'].length, 2, 'there should be 2 outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('foo and bar outbound', function () {
-	var out = req.outbound(['./foo', './bar']);
+	var out = req.outbound(['amd/foo', 'amd/bar']);
 	gt.equal(Object.keys(out).length, 2, 'there should be two outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./foo'].length, 1, "should be single element array, not " + JSON.stringify(out));
 	// gt.equal(out['./bar'].length, 0, "should be empty array, not " + JSON.stringify(out));
 });
 
 gt.test('uniques', function () {
-	var out = req.outbound(['./foo', './foo', './foo']);
+	var out = req.outbound(['amd/foo', 'amd/foo', 'amd/foo']);
 	gt.equal(Object.keys(out).length, 1, '3 identical module names collapsed to 1');
 });
 
 gt.test('uniques 2', function () {
-	var out = req.outbound(['./foo', './foo', './foo']);
+	var out = req.outbound(['amd/foo', 'amd/foo', 'amd/foo']);
 	gt.equal(Object.keys(out).length, 1, '3 identical module names collapsed to 1');
 });
 sure.run();
--- test/test-basic.js
+++ test/test-basic.js
@@ -1,6 +1,6 @@
-var sure = require('../../node_modules/gt/sure.js');
-var req = require('../../req-count');
-var check = require('../../node_modules/check-types');
+var sure = require('../node_modules/gt/sure.js');
+var req = require('../req-count');
+var check = require('../node_modules/check-types');
 
 sure.init({
 	report: 1
@@ -17,45 +17,45 @@ gt.test('basic', function () {
 });
 
 gt.test('foo outbound', function () {
-	var out = req.outbound('./foo');
+	var out = req.outbound('basic/foo');
 	gt.ok(out, 'returns an object');
 	gt.equal(Object.keys(out).length, 1, 'there should be single outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./foo'].length, 1, "should be single element array, not " + JSON.stringify(out));
 });
 
 gt.test('bar outbound', function () {
-	var out = req.outbound('./bar');
+	var out = req.outbound('basic/bar');
 	gt.equal(Object.keys(out).length, 1, 'there should be nothing outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./bar'].length, 0, "should be single empty array, not " + JSON.stringify(out));
 });
 
 gt.test('zoo outbound', function () {
-	var out = req.outbound('./zoo');
+	var out = req.outbound('basic/zoo');
 	gt.ok(out, 'could not get result');
 	// gt.equal(out['./zoo'].length, 1, 'there should be single outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('two outbound', function () {
-	var out = req.outbound('./two');
+	var out = req.outbound('basic/two');
 	gt.equal(Object.keys(out).length, 1, 'there should be 2 outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./two'].length, 2, 'there should be 2 outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('multiples do not count', function () {
-	var out = req.outbound('./multiples');
+	var out = req.outbound('basic/multiples');
 	gt.equal(Object.keys(out).length, 1, 'there should be 1 outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./multiples'].length, 2, 'there should be 2 outbound, not ' + JSON.stringify(out));
 });
 
 gt.test('foo and bar outbound', function () {
-	var out = req.outbound(['./foo', './bar']);
+	var out = req.outbound(['basic/foo', 'basic/bar']);
 	gt.equal(Object.keys(out).length, 2, 'there should be two outbound, not ' + JSON.stringify(out));
 	// gt.equal(out['./foo'].length, 1, "should be single element array, not " + JSON.stringify(out));
 	// gt.equal(out['./bar'].length, 0, "should be empty array, not " + JSON.stringify(out));
 });
 
 gt.test('uniques', function () {
-	var out = req.outbound(['./foo', './foo', './foo']);
+	var out = req.outbound(['basic/foo', 'basic/foo', 'basic/foo']);
 	gt.equal(Object.keys(out).length, 1, '3 identical module names collapsed to 1');
 })
 
